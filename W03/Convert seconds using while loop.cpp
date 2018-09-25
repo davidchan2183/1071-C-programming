98139696dc7d0e1f38dad177591b0548de4cba8f
davidchan2183 <43335102+davidchan2183@users.noreply.github.com>
2018-09-25 18:24:28 +0800

    Add files via upload

---
+++ W03/Convert seconds using while loop.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{ 
+   int sec;
+   int hh,mm,ss;
+   printf("Enter seconds:");
+   scanf("%d",&sec);
+   while(sec>=0){
+        hh=sec/3600;
+        mm=sec%3600/60;
+        ss=sec%60;
+        printf("%d sec=%02d:%02d:%02d\n",sec,hh,mm,ss);
+   printf("Enter seconds:");
+   scanf("%d",&sec);
+   } 
+}
+/*Enter seconds: 20000
+20000 seconds = 05:33:20*/
---
+++ W03/sum.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+	int n1, n2;
+	int sum=0;
+	for(int n1=1;n1<=n2;n1++){
+	sum+=n1
+	}
+}
+
